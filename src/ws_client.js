69fe204e0a789fbabbe5bbe8599a3d3fe0c8355e
thomas <thomas.bereziat@cea.fr>
2019-07-08 04:41:30 +0200

    la communication est ok entre arduino server.js et index.html

--- src/ws_client.js
+++ src/ws_client.js
@@ -1,11 +1,12 @@
 // client.js
 
 const WebSocket = require('ws')
-const url = 'ws://localhost:8080'
+const url = 'ws://192.168.50.1:3000'
 const connection = new WebSocket(url)
 
-
+console.log("ws_client");
 connection.onopen = (e) => {
+    console.log("port ouvert");
 }
 
 connection.onerror = (error) => {
@@ -14,6 +15,6 @@ connection.onerror = (error) => {
 
 connection.onmessage = (e) => {
   console.log(e.data);
-  connection.send('Message From Client ' + e.data); 
+  connection.send((JSON.stringify({"test": "test"}))); 
   
 }
--- src/ws_server.js
+++ src/ws_server.js
@@ -1,20 +1,74 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws').Server;
+const express = require('express');
+var cors = require('cors')
 
-const wss = new WebSocket.Server({ port: 8080 });
+var app = express()
+app.use(cors())
+
+app.use(express.static('public'));
+
+
 var nbr = 1;
+var hello = "hello";
+var buf;
+var jsonContent;
 
+var wss = new WebSocket({ port: 8080 });
+console.log("serveur démarré..."); 
 
-function envoi(){
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('recu par le client: %s', message);
-    ws.send(nbr + "from server ");
-    nbr++;
-  });
-
-  
+    wss.on('connection', function (ws) {
+      
+       ws.send("send 1 : " +buf);
+      ws.on('message', function (message) {
+        buf = message;
+        console.log(message);
+        wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === ws.OPEN) {
+            client.send("send 2 : " + message);
+          }
+        });
+    
+        //ws.broadcast.send("send 2 : " + message.data );
+        //console.log("test");
+      
+        try{
+          JSON.parse(message);
+//          console.log('recu par le client: %s', message);
+//            ws.send("message" + message);
+          
+          
+          
+        nbr++;
+          
+        }catch (e){
+          console.log("erreur de reception");
+        }  
+        
+      });
+     
 });
 
-}
 
-envoi();
+
+
+
+
+app.get('/index.html', function(req, res) {
+//      res.setHeader('Content-Type', 'text/plain');
+//      res.send(buf);
+//      res.json({msg: 'This is CORS-enabled for all origins!'})
+      console.log("server : " + buf);
+      });
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
