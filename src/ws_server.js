0b3119ccc161410272fc56abd59d0c5dbe02dd20
thomas <thomas.bereziat@cea.fr>
2019-07-19 10:09:33 +0200

    version stable

--- src/ws_server.js
+++ src/ws_server.js
@@ -1,56 +1,76 @@
-const WebSocket = require('ws').Server;
+const WebSocket = require('ws').Server
+let request = require('request');
+const MongoClient = require('mongodb').MongoClient, assert = require('assert');
 const express = require('express');
-var cors = require('cors')
+
+// init openweather
+let apiKey = 'e0d3505562b0333fb73fd10f0e9dac52';
+let city = 'toulouse';
+let urlw = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+var url = 'mongodb://localhost:27017/myproject';
 
 var app = express()
-app.use(cors())
 
 app.use(express.static('public'));
 
 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db.createCollection("Raspi");
+  db.close();
+});
 var nbr = 1;
 var hello = "hello";
 var buf;
 var jsonContent;
-
+var defautReception = 0;
 var wss = new WebSocket({ port: 8080 });
+var weather;
+
+
 console.log("serveur démarré..."); 
 
     wss.on('connection', function (ws) {
       
-       ws.send("send 1 : " +buf);
+       ws.send(buf);
       ws.on('message', function (message) {
-        buf = message;
-        console.log(message);
-        wss.clients.forEach(function each(client) {
+        
+        // test erreur de reception
+        try {
+          wss.clients.forEach(function each(client) {
           if (client !== ws && client.readyState === ws.OPEN) {
-            client.send("send 2 : " + message);
+            console.log(message);
+            
+            client.send(message);
           }
-        });
-    
-        //ws.broadcast.send("send 2 : " + message.data );
-        //console.log("test");
-      
-        try{
-          JSON.parse(message);
-//          console.log('recu par le client: %s', message);
-//            ws.send("message" + message);
+        }); 
+        //let temp  = JSON.parse(message);
+        //reception(message);
+        }catch (e) {
+          defautReception++;
+          console.log("defaut reception N°" + defautReception);
+        }
+         
+        buf = message;
           
-          
-          
-        nbr++;
-          
-        }catch (e){
-          console.log("erreur de reception");
-        }  
         
       });
      
 });
 
-
-
-
+function reception(message){
+  
+  wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === ws.OPEN) {
+            console.log(message);
+            
+            client.send(message);
+          }
+        });  
+  
+}
 
 
 app.get('/index.html', function(req, res) {
