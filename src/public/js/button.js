aa4e602a20b2e547ee36b20aadd81098c5b1d802
thomas <thomas.bereziat@cea.fr>
2019-07-31 17:16:10 +0200

    mise en place du nouveau graph

--- src/public/js/button.js
+++ src/public/js/button.js
@@ -12,25 +12,26 @@
       
       // gestion button start
       $(function(){
-	$("#saveButton").click(function(){
-	  $(this).attr("disabled","disabled");
-	  $("#stopButton").removeAttr("disabled");
-	  console.log(testSave);
-	  //Date beginSave = new Date()
-	  let myJson = JSON.stringify( {"req" : "SAVE" , "date" : new Date()});
-	  ws.send(myJson);
-	});
+          $("#saveButton").click(function(){
+              $(this).attr("disabled","disabled");
+              $("#stopButton").removeAttr("disabled");
+              console.log(testSave);
+              //Date beginSave = new Date()
+              let myJson = JSON.stringify( {"req" : "SAVE" , "date" : new Date()});
+              ws.send(myJson);
+          });
       });
     
       //gestion button stop
       $(function(){
-	$("#stopButton").click(function(){
-	$(this).attr("disabled","disabled");
-	$("#saveButton").removeAttr("disabled");
-	console.log(testStop);
-	let myJson = JSON.stringify( {"req" : "STOP" , "date" : new Date()});
-	  ws.send(myJson);
-	});
+          $("#stopButton").click(function(){
+              $(this).attr("disabled","disabled");
+              $("#saveButton").removeAttr("disabled");
+              console.log(testStop);
+              let myJson = JSON.stringify( {"req" : "STOP" , "date" : new Date()});
+                ws.send(myJson);
+               
+          });
       });
 
     function stopData(){
@@ -39,3 +40,17 @@
       console.log(test);
       ws.send(test);
     }
+
+    $(function(){
+      $('button').click(function(event){
+
+          // $(this).event.attr.
+          // $("#stopButton").removeAttr("disabled");
+          // console.log(testSave);
+          // //Date beginSave = new Date()
+          // let myJson = JSON.stringify( {"req" : "SAVE" , "date" : new Date()});
+          // ws.send(myJson);
+          alert("ouf!");
+      });
+  });
+ 
